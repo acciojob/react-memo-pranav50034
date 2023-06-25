5573b1533d3070ecb4983fd04484d63e399c51fb
Pranav Pokale <pranavpokale50034@gmail.com>
2023-06-25 17:55:59 +0530

    submit

---
+++ src/components/Todo.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+const Todo = () => {
+   let [todos, setTodos] = useState(["New Todo"]);
+
+   return (
+      <div>
+         <h2>My todos</h2>
+         {todos && todos.map((todo, ind) => <p id={`todo-${ind}`}>{todo}</p>)}
+         <button
+            id="add-todo-btn"
+            onClick={() => {
+               setTodos([...todos, "New todo"]);
+            }}
+         >
+            Add Todo
+         </button>
+         <br />
+         <br />
+         <hr />
+      </div>
+   );
+};
+
+export default Todo;
